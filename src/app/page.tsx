7ae6bca7faafc5323891b5d2f4ca73d00b698f90
MiguelASFerreira <migant1306@gmail.com>
2025-06-27 17:49:51 -0300

    refatorar: adicionar configuração de imagens e animação de digitação ao layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <h1>
-      Hello World
-    </h1>
+    <div className="flex px-[200px]">
+      <div className="absolute top-[144px] left-[200px]">
+        <Image
+          src="https://avatars.githubusercontent.com/u/106342952?v=4"
+          alt="Miguel Antonio"
+          width={400}
+          height={667}
+          className="rounded-[40px]"
+        />
+      </div>
+      <div className="absolute top-[300px] left-[722px]">
+        <h1 className="font-lobster text-[40px] text-white">
+          Miguel Antonio Santos Ferreira!
+        </h1>
+          <div>
+            <p className="text-3xl text-white font-lobster typing-animation max-w-max">
+              Desenvolvedor FullStack
+            </p>
+          </div>
+      </div>
+    </div>
   );
 }
