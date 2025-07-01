091e8ab3e878080964a935c0c76adecccb650a57
MiguelASFerreira <migant1306@gmail.com>
2025-07-01 11:31:47 -0300

    refactor: ajustar dimensões da imagem e simplificar a estrutura do componente Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center gap-6 px-4 py-12 lg:flex-row lg:items-center lg:gap-12 lg:px-[200px]">
+    <div className="flex flex-col items-center justify-center gap-6 px-4 py-12">
       <div>
         <Image
           src="https://avatars.githubusercontent.com/u/106342952?v=4"
           alt="Miguel Antonio"
-          width={400}
-          height={667}
-          className="h-auto w-full max-w-[280px] rounded-[40px] md:max-w-[320px] lg:max-w-[400px]"
+          width={300}
+          height={300}
+          className="rounded-[40px]"
           quality={100}
         />
       </div>
 
-      <div className="text-center lg:text-left">
+      <div className="text-center">
         <h1 className="font-lobster text-[28px] leading-tight text-white md:text-[36px] lg:text-[40px]">
           Miguel Antonio Santos Ferreira!
         </h1>
