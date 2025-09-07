5f2afb9829abfbbf5e93ce9fb3e198b40badc1cb
MiguelASFerreira <migant1306@gmail.com>
2025-09-07 12:13:40 -0300

    feat: add A.J Rorato experience entry and logo

--- src/configs/experience.ts
+++ src/configs/experience.ts
@@ -10,6 +10,16 @@ export interface ExperienceEntry {
 
 export const experience: ExperienceEntry[] = [
   {
+    title: "A.J Rorato - Jovem Aprendiz",
+    description:
+      "Atuei como jovem aprendiz, participando de atividades em sala de aula voltadas para o desenvolvimento profissional e, posteriormente, desempenhei funções na linha de produção, sendo responsável pelo processo de embalamento de armários de banheiro, sempre seguindo os padrões de qualidade estabelecidos e desenvolvendo habilidades como organização, disciplina e trabalho em equipe.",
+    startDate: "Ago. 2025",
+    endDate: "Set. 2025",
+    isActive: false,
+    logo: "/logo/logo-ajrorato.svg",
+    companyUrl: "https://www.ajrorato.ind.br/",
+  },
+  {
     title: "Viptech Smart Solutions - Estágio",
     description:
       "Atuei como estagiário de desenvolvimento Full Stack, com carga de 30 horas semanais, participando ativamente de projetos internos e externos. Contribuí para o desenvolvimento e manutenção de funcionalidades, identificação e correção de bugs, além da implementação de soluções alinhadas a requisitos técnicos e de negócios. Trabalhei com múltiplas linguagens e ambientes, focando em módulos escaláveis e eficientes, sempre em colaboração com times multidisciplinares. Ao longo do estágio, aprimorei habilidades em análise de sistemas, boas práticas de programação e resolução de problemas em ambientes ágeis.",
@@ -27,7 +37,7 @@ export const experience: ExperienceEntry[] = [
     endDate: "Fev. 2025",
     isActive: false,
     logo: "/logo/logo-integrado.svg",
-    companyUrl: 'https://www.grupointegrado.br/',
+    companyUrl: "https://www.grupointegrado.br/",
   },
   {
     title: "Grupo Integrado - Estágio",
@@ -37,6 +47,6 @@ export const experience: ExperienceEntry[] = [
     endDate: "Set. 2024",
     isActive: false,
     logo: "/logo/logo-integrado.svg",
-    companyUrl: 'https://www.grupointegrado.br/',
+    companyUrl: "https://www.grupointegrado.br/",
   },
 ];
